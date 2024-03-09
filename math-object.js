0f1cb5d069f173437ac6143409699da6f0519da1
Mosharof <md5470415@gmail.com>
2024-03-09 22:41:24 +0600

    dropdown menu design

---
+++ math-object.js
@@ -0,0 +1,13 @@
+// Math Objects 
+
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.abs(-5));
+console.log(Math.floor(4.9));
+console.log(Math.ceil(4.01));
+console.log(Math.round(7.5))
+console.log(Math.round(7.235))
+console.log(Math.trunc(8.56234));
+
+console.log(Math.random() * 3452) + 5257;
+
