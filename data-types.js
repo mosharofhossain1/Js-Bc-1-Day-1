ea8b7a12cb14216c95692b22064e67a3389f4ac2
Mosharof <md5470415@gmail.com>
2024-03-09 19:16:01 +0600

    js Data TYpes learn

---
+++ data-types.js
@@ -0,0 +1,27 @@
+// Js Data Types 
+
+// JavaScript has several built-in data types, which can be broadly categorized into two categories: primitive data types and non-primitive (reference) data types. Below are the data types in JavaScript:
+
+// Number 1: Primitive Data type- [String, Number, Boolean, Undifined, Null.Undifiend]
+// Number 2: Non-Primitive Data / Referance Data Type-[Object, Array, Function, Date, RegExp] 
+
+//  Primitve Data Types Explain 
+
+// 1: Number Type Data :
+let number =  234;
+console.log(number)
+// 2: String Type Data :
+let friendsName = 'Digonto Sorker';
+console.log(friendsName);
+// 3: Boolean Type Data:
+let isWinter = false;
+console.log(isWinter)
+// 4: Null TYpe Data 
+
+let noData = null;
+console.log(noData);
+
+// 5: Undifiend Data Type
+
+let undi = undefined;
+console.log(undi)
